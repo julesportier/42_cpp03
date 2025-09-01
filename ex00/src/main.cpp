6fa34eb308d0973aeef30f03cad15df4713ea9d3
jules <142106821+julesportier@users.noreply.github.com>
2025-09-01 18:39:04 +0200

    test(ex00): actually call copy assignement operator

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -20,26 +20,27 @@ int main()
 	ct.beRepaired(10);
 	ct.beRepaired(std::numeric_limits<unsigned int>::max());
 
-	// ClapTrap ct_bis = ct;
-	// std::cout
-	// 	<< "-- ct --"
-	// 	<< "\nname: " << ct.getName()
-	// 	<< "\nhealth: " << ct.getHealth()
-	// 	<< "\nenergy: " << ct.getEnergy()
-	// 	<< "\nattackDamage: " << ct.getAttackDamage()
-	// 	<< "\n--------\n";
-	// ct_bis.takeDamage(1);
-	// ct.getHealth();
+	ClapTrap ct_bis = ct;
+	std::cout
+		<< "-- ct_bis --"
+		<< "\nname: " << ct_bis.getName()
+		<< "\nhealth: " << ct_bis.getHealth()
+		<< "\nenergy: " << ct_bis.getEnergy()
+		<< "\nattackDamage: " << ct_bis.getAttackDamage()
+		<< "\n--------\n";
+	ct_bis.takeDamage(1);
+	ct.getHealth();
 
-	// ClapTrap ct_ter = ClapTrap(ct_bis);
-	// std::cout
-	// 	<< "-- ct --"
-	// 	<< "\nname: " << ct.getName()
-	// 	<< "\nhealth: " << ct.getHealth()
-	// 	<< "\nenergy: " << ct.getEnergy()
-	// 	<< "\nattackDamage: " << ct.getAttackDamage()
-	// 	<< "\n--------\n";
-	// ct_ter.takeDamage(1);
-	// ct_bis.getHealth();
+	ClapTrap ct_ter = ClapTrap("ter");
+	ct_ter = ct_bis;
+	std::cout
+		<< "-- ct_ter --"
+		<< "\nname: " << ct_ter.getName()
+		<< "\nhealth: " << ct_ter.getHealth()
+		<< "\nenergy: " << ct_ter.getEnergy()
+		<< "\nattackDamage: " << ct_ter.getAttackDamage()
+		<< "\n--------\n";
+	ct_ter.takeDamage(1);
+	ct_bis.getHealth();
 	return (0);
 }
